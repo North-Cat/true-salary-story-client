09de87c54ded5a7647a5a7380642b37f2572e649
May.Yang <may.yang@hy-tech.com.tw>
2023-04-18 16:22:26 +0800

    chore: init tailwind css

--- app.vue
+++ app.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <!-- <NuxtWelcome /> -->
+
+    <!-- <NuxtPage /> 用於路由設定，會根據 pages/ 資料夾底下的檔案名稱作為路由 -->
+    <!-- 例如 "/about" 會載入 "pages/about.vue"，而 "/" 預設載入 "pages/index.vue"  -->
+    <NuxtPage /> 
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+    postcss: {
+        plugins: {
+            'postcss-import': {},
+            'tailwindcss/nesting': {},
+            tailwindcss: {},
+            autoprefixer: {}
+        }
+    },
+    css: ['@/assets/css/tailwind.css']
 })
