deecb4fad3041373558df62cc8d54ba6a5e366a7
jung0730 <arj2763@gmail.com>
2023-05-16 17:51:22 +0800

    style: 調整salary-keyword

--- components/salary-keyword.vue
+++ components/salary-keyword.vue
@@ -1,8 +1,14 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div
-    class="border border-black-1 bg-white lg:w-2/6 flex flex-col justify-start items-start py-6 px-3 lg:ml-[30px] h-fit">
-    keyword
+  <div class="lg:w-2/6 flex flex-col justify-start items-start py-6 px-3 lg:ml-[30px] h-fit">
+    <h5 class="text-white bg-black-10 p-6 rounded-t">熱門關鍵字</h5>
+    <div
+      class="border-2 rounded-tr rounded-br rounded-bl w-full flex flex-col justify-start items-start py-6 px-3 h-fit"
+    >
+      keyword
+    </div>
   </div>
 </template>
+
+<style></style>
