e51f7081127971679040ef0b925753d12f67030a
jung0730 <arj2763@gmail.com>
2023-05-25 10:46:59 +0800

    style: [salaryId].vue

--- pages/salary/[salaryId].vue
+++ pages/salary/[salaryId].vue
@@ -4,15 +4,13 @@ useHead({
 });
 </script>
 <template>
-  <div>
-    <div class="bg-gray pt-[130px] pb-10 md:py-20 max-[1920px]:overflow-x-hidden min-h-screen">
-      <div
-        class="md-container mx-auto sm:max-w-[500px] md:max-w-[600px] lg:max-w-7xl flex flex-col justify-center items-center lg:mt-20"
-      >
-        <div class="w-full flex sm:flex-col lg:flex-row lg:justify-between">
-          <SalaryView />
-          <SalaryKeyword />
-        </div>
+  <div class="bg-gray pt-[130px] pb-10 md:py-20 max-[1920px]:overflow-x-hidden min-h-screen">
+    <div
+      class="md-container mx-auto sm:max-w-[500px] md:max-w-[600px] lg:max-w-7xl flex flex-col justify-center items-center lg:mt-20"
+    >
+      <div class="w-full flex sm:flex-col lg:flex-row lg:justify-between">
+        <SalaryView />
+        <SalaryKeyword />
       </div>
     </div>
   </div>
