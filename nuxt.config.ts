fcb036976a71812f3e1defd1e2bb30b3afa611e5
Pitt Wu <kgb00128@gmail.com>
2023-06-05 10:09:45 +0800

    fix: open & update pwa plugin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,9 @@
+import { defineNuxtConfig } from 'nuxt/config';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   // when open pwa add this module '@vite-pwa/nuxt'
-  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt', '@vueuse/nuxt', 'nuxt-gtag'],
+  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt', '@vueuse/nuxt', 'nuxt-gtag', '@vite-pwa/nuxt'],
   build: { transpile: ['yup', 'lodash', '@vee-validate/rules'] },
   runtimeConfig: {
     public: {
@@ -17,72 +19,72 @@ export default defineNuxtConfig({
   gtag: {
     id: 'G-KBE06V0GZ4',
   },
-  // pwa: {
-  //   registerType: 'autoUpdate',
-  //   manifest: {
-  //     name: '真薪話',
-  //     short_name: '真薪話',
-  //     description: '最真實的薪水行情 Web App',
-  //     display: 'standalone',
-  //     theme_color: '#366BAB',
-  //     icons: [
-  //       {
-  //         src: 'pwa/pwa-icon-64x64.png',
-  //         sizes: '64x64',
-  //         type: 'image/png',
-  //       },
-  //       {
-  //         src: 'pwa/pwa-icon-144x144.png',
-  //         sizes: '144x144',
-  //         type: 'image/png',
-  //       },
-  //       {
-  //         src: 'pwa/pwa-icon-192x192.png',
-  //         sizes: '192x192',
-  //         type: 'image/png',
-  //       },
-  //       {
-  //         src: 'pwa/pwa-icon-512x512.png',
-  //         sizes: '512x512',
-  //         type: 'image/png',
-  //       },
-  //       {
-  //         src: 'pwa-512x512.png',
-  //         sizes: '512x512',
-  //         type: 'image/png',
-  //         purpose: 'any maskable',
-  //       },
-  //       {
-  //         src: 'pwa/apple-touch-icon.png',
-  //         sizes: '180x180',
-  //         type: 'image/png',
-  //       },
-  //       {
-  //         src: 'pwa/safari-pinned-tab.svg',
-  //         type: 'svg',
-  //       },
-  //     ],
-  //   },
-  //   workbox: {
-  //     runtimeCaching: [
-  //       {
-  //         urlPattern: /.*\.(?:css|js)/,
-  //         handler: 'NetworkFirst',
-  //       },
-  //     ],
-  //     navigateFallback: '/',
-  //     globPatterns: ['**/*.{js,ts,vue,css,html,png,svg,ico}'],
-  //   },
-  //   // TODO: Temp Test for PWA
-  //   // client: {
-  //   //   installPrompt: true,
-  //   //   periodicSyncForUpdates: 20,
-  //   // },
-  //   devOptions: {
-  //     enabled: true,
-  //     type: 'module',
-  //   },
-  // },
+  pwa: {
+    registerType: 'autoUpdate',
+    manifest: {
+      name: '真薪話',
+      short_name: '真薪話',
+      description: '最真實的薪水行情 Web App',
+      display: 'standalone',
+      theme_color: '#366BAB',
+      icons: [
+        {
+          src: 'pwa/pwa-icon-64x64.png',
+          sizes: '64x64',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa/pwa-icon-144x144.png',
+          sizes: '144x144',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa/pwa-icon-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa/pwa-icon-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any maskable',
+        },
+        {
+          src: 'pwa/apple-touch-icon.png',
+          sizes: '180x180',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa/safari-pinned-tab.svg',
+          type: 'svg',
+        },
+      ],
+    },
+    workbox: {
+      runtimeCaching: [
+        {
+          urlPattern: /.*\.(?:css|js)/,
+          handler: 'NetworkFirst',
+        },
+      ],
+      navigateFallback: '/',
+      globPatterns: ['**/*.{js,ts,vue,css,html,png,svg,ico}'],
+    },
+    // TODO: Temp Test for PWA
+    // client: {
+    //   installPrompt: true,
+    //   periodicSyncForUpdates: 20,
+    // },
+    devOptions: {
+      enabled: true,
+      type: 'module',
+    },
+  },
 
   // nitro: {
   //   devProxy: {
