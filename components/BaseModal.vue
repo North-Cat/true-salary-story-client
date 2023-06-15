136a79828d117c4835fd29c7a389c6dc0b481777
jung0730 <arj2763@gmail.com>
2023-06-15 14:12:04 +0800

    feat: 共用BaseModal 元件

---
+++ components/BaseModal.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    isVisible: boolean;
+  }>(),
+  {
+    isVisible: false,
+  },
+);
+const emit = defineEmits(['close']);
+</script>
+
+<template>
+  <teleport to="body">
+    <div v-if="isVisible" class="z-10 fixed inset-0 flex items-center justify-center bg-black-3 bg-opacity-50">
+      <div class="w-full max-w-lg p-6 bg-white rounded-md shadow-xl">
+        <div class="flex items-center justify-between">
+          <slot name="title" />
+          <button class="px-2 py-1 text-sm tracking-widest" @click="emit('close')">
+            <i class="icon-cross text-lg"></i>
+          </button>
+        </div>
+        <div class="my-4 body">
+          <slot name="body" />
+        </div>
+      </div>
+    </div>
+  </teleport>
+</template>
--- components/EmailModal.vue
+++ components/EmailModal.vue
@@ -56,51 +56,42 @@ const countDownTimer = () => {
 </script>
 
 <template>
-  <teleport to="body">
-    <div v-if="isVisible" class="z-10 fixed inset-0 flex items-center justify-center bg-black-3 bg-opacity-50">
-      <div class="w-full max-w-lg p-6 bg-white rounded-md shadow-xl">
-        <div class="flex items-center justify-between">
-          <h4>變更通知信箱</h4>
-          <button class="px-2 py-1 text-sm tracking-widest" @click="closeHandler">
-            <i class="icon-cross text-lg"></i>
-          </button>
-        </div>
-        <div class="mt-4">
-          <div class="mb-4 body">
-            <div class="min-h-[130px]">
-              <label for="email" class="text-black-10">通知信箱</label>
-              <input
-                v-model.trim="email"
-                name="email"
-                label="通知信箱"
-                type="text"
-                class="w-full border border-black-1 rounded py-2 px-4 mt-2"
-              />
-              <p v-show="hasError" class="text-red">Email 格式錯誤</p>
-            </div>
-            <div class="flex justify-end">
-              <BaseButton cate="secondary" :disabled="isTimerBtn" @click="onNext">{{
-                isCodeSent ? `取得驗證碼(${countDown})` : '取得驗證碼'
-              }}</BaseButton>
-            </div>
-            <template v-if="isCodeSent">
-              <label for="code" class="text-black-10">驗證碼</label>
-              <input
-                v-model.trim="code"
-                name="code"
-                label="驗證碼"
-                maxlength="4"
-                placeholder="- - - -"
-                type="text"
-                class="text-center w-full border border-black-1 rounded py-2 px-4 mt-2 mb-4"
-              />
-              <div class="flex flex-col">
-                <BaseButton cate="primary" :disabled="!code.length" @click="updateHandler">變更通知信箱</BaseButton>
-              </div>
-            </template>
-          </div>
-        </div>
+  <BaseModal :is-visible="isVisible" @close="closeHandler">
+    <template #title>
+      <h4>變更通知信箱</h4>
+    </template>
+    <template #body>
+      <div class="min-h-[130px]">
+        <label for="email" class="text-black-10">通知信箱</label>
+        <input
+          v-model.trim="email"
+          name="email"
+          label="通知信箱"
+          type="text"
+          class="w-full border border-black-1 rounded py-2 px-4 mt-2"
+        />
+        <p v-show="hasError" class="text-red">Email 格式錯誤</p>
       </div>
-    </div>
-  </teleport>
+      <div class="flex justify-end">
+        <BaseButton cate="secondary" :disabled="isTimerBtn" @click="onNext">{{
+          isCodeSent ? `取得驗證碼(${countDown})` : '取得驗證碼'
+        }}</BaseButton>
+      </div>
+      <template v-if="isCodeSent">
+        <label for="code" class="text-black-10">驗證碼</label>
+        <input
+          v-model.trim="code"
+          name="code"
+          label="驗證碼"
+          maxlength="4"
+          placeholder="- - - -"
+          type="text"
+          class="text-center w-full border border-black-1 rounded py-2 px-4 mt-2 mb-4"
+        />
+        <div class="flex flex-col">
+          <BaseButton cate="primary" :disabled="!code.length" @click="updateHandler">變更通知信箱</BaseButton>
+        </div>
+      </template>
+    </template>
+  </BaseModal>
 </template>
