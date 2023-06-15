f3295c1d0231f5c424188a3e308a023ef92a0305
jung0730 <arj2763@gmail.com>
2023-06-15 14:17:23 +0800

    style: naming

--- app.vue
+++ app.vue
@@ -16,16 +16,15 @@ onMounted(() => {});
   <div>
     <VitePwaManifest v-if="pwaEnv !== 'local'" />
     <!-- modal -->
-    <default-modal></default-modal>
+    <DefaultModal />
     <!-- message -->
     <client-only>
       <notifications position="top right" class="my-notification" :duration="3000" :max="5" />
     </client-only>
     <!-- 簽到提示 -->
-    <RemindCheckIn></RemindCheckIn>
+    <RemindCheckIn />
     <!-- 全畫面 loading -->
-    <PageLoading></PageLoading>
-
+    <PageLoading />
     <!-- 套用共通 layout (nav 跟 footer)-->
     <NuxtLayout>
       <NuxtLoadingIndicator :throttle="0" color="repeating-linear-gradient(to right,#34cdfe 0%,#366BAB 100%)" />
